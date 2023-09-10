6495e500842de802bd82bb2241507b2cc1a47691
burenkoia@oooinex.ru <burenkoia@oooinex.ru>
2023-09-10 11:48:19 +0300

    init

---
+++ gitlab/createDIr.sh
@@ -0,0 +1,11 @@
+minikube ssh
+
+sudo -s
+
+mkdir /var/lib/gitlab/
+mkdir /var/lib/gitlab/config
+mkdir /var/lib/gitlab/data
+mkdir /var/lib/gitlab/logs
+
+mkdir /var/lib/gitlab/runner/
+mkdir /var/lib/gitlab/runner/config
